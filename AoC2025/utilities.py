0ec57915f4ecc9f37da5cab8afa69b7c09d89c11
JV_git <volmajer.jerneja@gmail.com>
2025-12-01 18:43:02 +0100

    advent of code, star 1, attempt at star 2

---
+++ AoC2025/utilities.py
@@ -0,0 +1,26 @@
+def read_file_into_lines(file_path):
+    #this file will be recognised as a module that you can import into your working notebook.
+    #import utilities
+    #utilities.read_file_into_lines
+    """
+    A simple function to read a file into
+    a list of lines, removing the newline
+    character at the end.
+
+    Arguments:
+    ==========
+    file_path: str
+        The location of a text file to read.
+
+    Returns:
+    ========
+    list[str]
+        A list that contains all stripped lines
+        of the input text file.
+    """
+    lines = []
+    with open(file_path, 'r') as infile:
+        for line in infile.readlines():
+            cleaned = line.rstrip()
+            lines.append(cleaned)
+    return lines
