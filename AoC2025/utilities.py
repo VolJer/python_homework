a5eb6d5ea0c99852236a058dfc35166831012999
JV_git <volmajer.jerneja@gmail.com>
2025-12-14 13:33:02 +0100

    AoC entries, some completed, some not

--- AoC2025/utilities.py
+++ AoC2025/utilities.py
@@ -24,3 +24,10 @@ def read_file_into_lines(file_path):
             cleaned = line.rstrip()
             lines.append(cleaned)
     return lines
+
+def file_to_list(path):
+    lines = []
+    with open(path, 'r') as infile:
+        for line in infile.readlines():
+            lines.append(line.strip())
+    return lines
